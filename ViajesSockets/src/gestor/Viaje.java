abb4f051214d901bd3722eac51031be9b39b2702
elias <al405647@uji.es>
2022-10-18 10:40:33 +0200

    Initial commit

---
+++ ViajesSockets/src/gestor/Viaje.java
@@ -0,0 +1,13 @@
+package gestor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Vector;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class Viaje implements Serializable {
+	// Sustituye esta clase por tu versión de la misma de la práctica 1
+} // fin clase Viaje
